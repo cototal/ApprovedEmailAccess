32f412adc20951a12981d7d1d0a0bc6416407f82
Shaun T Martin <webmaster@cototal.com>
2019-07-20 16:25:22 -0500

    Allow separator to be configured.

--- Cototal.AspNetCore.ApprovedEmailAccess/Middleware/VerifyAdminEmailOptions.cs
+++ Cototal.AspNetCore.ApprovedEmailAccess/Middleware/VerifyAdminEmailOptions.cs
@@ -1,10 +1,15 @@
 ﻿namespace Cototal.AspNetCore.ApprovedEmailAccess.Middleware
 {
+    /// <summary>
+    /// Options for the VerifyAdminEmail service
+    /// </summary>
     public class VerifyAdminEmailOptions : IVerifyAdminEmailOptions
     {
-        public VerifyAdminEmailOptions(string adminEmails)
+        /// <param name="adminEmails">List of emails you want to allow access</param>
+        /// <param name="separator">Separator used between emails in the list</param>
+        public VerifyAdminEmailOptions(string adminEmails, char separator = ';')
         {
-            AdminEmails = adminEmails.Split(';');
+            AdminEmails = adminEmails.Split(separator);
         }
 
         public string[] AdminEmails { get; set; }
